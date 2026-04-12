7df2125bc9c9fb1cdd266fb0f8c5cf9ba96b23d4
Pyritie <pyritie@gmail.com>
2026-04-12 11:48:44 +0100

    add more languages
    Signed-off-by: Pyritie <pyritie@gmail.com>

--- src/main/java/team/terrafirmgreg/fieldguide/localization/Language.java
+++ src/main/java/team/terrafirmgreg/fieldguide/localization/Language.java
@@ -8,10 +8,17 @@ import java.util.List;
 @Getter
 public enum Language {
     EN_US("en_us", "English"),
+    DE_DE("de_de", "Deutsch"),
+    ES_ES("es_es", "Español"),
+    FR_FR("fr_fr", "Français"),
+    HU_HU("hu_hu", "Magyar"),
     JA_JP("ja_jp", "日本語"),
     KO_KR("ko_kr", "한국어"),
+    PL_PL("pl_pl", "Polski"),
     PT_BR("pt_br", "Portuguese (Brazil)"),
     RU_RU("ru_ru", "Русский"),
+    SV_SE("sv_se", "Svenska"),
+    TR_TR("tr_tr", "Türkçe"),
     UK_UA("uk_ua", "Українська мова"),
     ZH_CN("zh_cn", "简体中文(中国大陆)"),
     ZH_HK("zh_hk", "繁體中文（港澳)"),
